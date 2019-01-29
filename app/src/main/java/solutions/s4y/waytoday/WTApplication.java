5559c36a1fe45340f38b01b5da8becb57770e5d9
Sergey Dolin <sergey@s4y.solutions>
2019-01-30 01:08:29 +0500

    Enable Strict Mode

--- app/src/main/java/solutions/s4y/waytoday/WTApplication.java
+++ app/src/main/java/solutions/s4y/waytoday/WTApplication.java
@@ -1,8 +1,25 @@
 package solutions.s4y.waytoday;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 public class WTApplication extends Application {
+    static {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+    }
     private AppComponent mAppComponent;
 
     @Override
