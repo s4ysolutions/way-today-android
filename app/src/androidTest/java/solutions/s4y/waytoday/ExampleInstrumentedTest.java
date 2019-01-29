d849af6e7795ebdee87336aff4d41909f7b83008
Sergey Dolin <sergey@s4y.solutions>
2019-01-30 00:43:37 +0500

    Switch to ext:junit

--- app/src/androidTest/java/solutions/s4y/waytoday/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/solutions/s4y/waytoday/ExampleInstrumentedTest.java
@@ -2,8 +2,8 @@ package solutions.s4y.waytoday;
 
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,12 +16,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
         assertEquals("solutions.s4y.waytoday", appContext.getPackageName());
     }
+
+    @Test
+    public void useAppContext2() {
+        // Context of the app under test.
+        Context appContext = ApplicationProvider.getApplicationContext();
+
+        assertEquals("solutions.s4y.waytoday", appContext.getPackageName());
+    }
+
 }
