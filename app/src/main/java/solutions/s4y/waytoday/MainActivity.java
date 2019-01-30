1c53864663f133c1d534231e83a6fc29c9474e96
Sergey Dolin <sergey@s4y.solutions>
2019-01-30 23:40:54 +0500

    Switch to Activity instead of SupportActivity

--- app/src/main/java/solutions/s4y/waytoday/MainActivity.java
+++ app/src/main/java/solutions/s4y/waytoday/MainActivity.java
@@ -1,9 +1,9 @@
 package solutions.s4y.waytoday;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
