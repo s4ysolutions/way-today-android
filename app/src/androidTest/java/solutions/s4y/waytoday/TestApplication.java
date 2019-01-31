ccadc74084ac7be51f735756d5d9765e4cddf2d6
Sergey Dolin <sergey@s4y.solutions>
2019-01-31 21:26:58 +0500

    TestApplication to inject test dagger modules

---
+++ app/src/androidTest/java/solutions/s4y/waytoday/TestApplication.java
@@ -0,0 +1,21 @@
+package solutions.s4y.waytoday;
+
+import android.content.Context;
+
+public class TestApplication extends WTApplication {
+
+
+    @Override
+    protected AppComponent prepareAppComponent() {
+        System.setProperty("org.mockito.android.target",
+                this
+                        .getDir("target", Context.MODE_PRIVATE)
+                        .getAbsolutePath());
+//                        .getCacheDir().getPath());
+
+        return DaggerTestComponent
+                .builder()
+                .applicationModule(new DaggerApplicationModule(this))
+                .build();
+    }
+}
