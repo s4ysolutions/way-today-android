93c84d22f8c6a1f62eac85cc940d104d7d265fd7
Sergey Dolin <sergey@s4y.solutions>
2019-01-31 21:27:40 +0500

    Custom test runner to use test application

--- app/src/androidTest/java/solutions/s4y/waytoday/Runner.java
+++ app/src/androidTest/java/solutions/s4y/waytoday/Runner.java
@@ -8,6 +8,6 @@ import androidx.test.runner.AndroidJUnitRunner;
 public class Runner extends AndroidJUnitRunner {
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        return super.newApplication(cl, WTApplication.class.getName(), context);
+        return super.newApplication(cl, TestApplication.class.getName(), context);
     }
 }
