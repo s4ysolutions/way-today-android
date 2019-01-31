cb22e27aa10efd915e4b50c53cf9b521beec0317
Sergey Dolin <sergey@s4y.solutions>
2019-01-31 13:07:10 +0500

    Add SharedPreferences provision

--- app/src/main/java/solutions/s4y/waytoday/ApplicationModule.java
+++ app/src/main/java/solutions/s4y/waytoday/ApplicationModule.java
@@ -1,6 +1,8 @@
 package solutions.s4y.waytoday;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import javax.inject.Singleton;
 
@@ -21,4 +23,9 @@ class ApplicationModule {
         return application;
     }
 
+    @Provides
+    @Singleton
+    SharedPreferences provideSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
 }
