4c6d3fe3309e4705539b13af4c3cf14b08f77e9a
Sergey Dolin <sergey@s4y.solutions>
2019-01-31 14:08:47 +0500

    Migrate ExampleInstrumentedTest to google truth and mockito

--- app/src/androidTest/java/solutions/s4y/waytoday/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/solutions/s4y/waytoday/ExampleInstrumentedTest.java
@@ -10,7 +10,9 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,24 +22,37 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 
 public class ExampleInstrumentedTest {
+    private class FooMocked {
+        void foo() {
+
+        }
+    }
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void useAppContext() {
+        FooMocked fooMocked = mock(FooMocked.class);
         // Context of the app under test.
         Context appContext = ApplicationProvider.getApplicationContext();
 
-        assertEquals("solutions.s4y.waytoday", appContext.getPackageName());
+        assertThat("solutions.s4y.waytoday").isEqualTo(appContext.getPackageName());
+//        assertEquals("solutions.s4y.waytoday", appContext.getPackageName());
+        fooMocked.foo();
+        verify(fooMocked).foo();
     }
 
     @Test
     public void useAppContext2() {
+        FooMocked fooMocked = mock(FooMocked.class);
         // Context of the app under test.
         Context appContext = ApplicationProvider.getApplicationContext();
 
-        assertEquals("solutions.s4y.waytoday", appContext.getPackageName());
+        assertThat("solutions.s4y.waytoday").isEqualTo(appContext.getPackageName());
+//        assertEquals("solutions.s4y.waytoday", appContext.getPackageName());
+        fooMocked.foo();
+        verify(fooMocked).foo();
     }
 
 }
