c8862c0ba79f3dffc547fa5b1c2d88695a18eb02
Sergey Dolin <sergey@s4y.solutions>
2019-01-31 15:36:35 +0500

    Add custom runner

---
+++ app/src/androidTest/java/solutions/s4y/waytoday/Runner.java
@@ -0,0 +1,13 @@
+package solutions.s4y.waytoday;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.test.runner.AndroidJUnitRunner;
+
+public class Runner extends AndroidJUnitRunner {
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        return super.newApplication(cl, WTApplication.class.getName(), context);
+    }
+}
