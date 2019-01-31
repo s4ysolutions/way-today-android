8bfa519d1c580c87c5540b8c40304651dc564f39
Sergey Dolin <sergey@s4y.solutions>
2019-01-31 13:20:24 +0500

    Make IDService injectable by Dagger

--- app/src/main/java/solutions/s4y/waytoday/AppComponent.java
+++ app/src/main/java/solutions/s4y/waytoday/AppComponent.java
@@ -4,17 +4,21 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import solutions.s4y.waytoday.errors.ErrorsModule;
+import solutions.s4y.waytoday.grpc.GRPCChannelProviderModule;
+import solutions.s4y.waytoday.idservice.IDService;
 import solutions.s4y.waytoday.preferences.PreferencesModule;
 
 @Singleton
 @Component(modules = {
         ApplicationModule.class,
         ErrorsModule.class,
-        PreferencesModule.class
+        PreferencesModule.class,
+        GRPCChannelProviderModule.class
 })
 
 public interface AppComponent {
     void inject(WTApplication application);
-
     void inject(MainActivity activity);
+    void inject(IDService service);
+
 }
