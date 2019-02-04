3f499c5049634181e572a8319e67630ef1ffd375
Sergey Dolin <sergey@s4y.solutions>
2019-02-04 20:25:27 +0500

    New features to MainActivity

--- app/src/main/java/solutions/s4y/waytoday/MainActivity.java
+++ app/src/main/java/solutions/s4y/waytoday/MainActivity.java
@@ -1,6 +1,11 @@
 package solutions.s4y.waytoday;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,6 +17,7 @@ import android.widget.TextView;
 
 import javax.inject.Inject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 import butterknife.BindView;
@@ -20,7 +26,9 @@ import butterknife.OnClick;
 import butterknife.OnTouch;
 import io.reactivex.disposables.CompositeDisposable;
 import solutions.s4y.waytoday.errors.ErrorsObservable;
+import solutions.s4y.waytoday.locations.LocationsService;
 import solutions.s4y.waytoday.mainactivity.FrequencyGestureListener;
+import solutions.s4y.waytoday.preferences.PreferenceIsTracking;
 import solutions.s4y.waytoday.preferences.PreferenceUpdateFrequency;
 import solutions.s4y.waytoday.sound.MediaPlayerUtils;
 
@@ -28,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private final static String LT = AppCompatActivity.class.getSimpleName();
     @Inject
     PreferenceUpdateFrequency mUserStrategyFrequency;
+    static boolean mHasFocus = false;
 
     @BindView(R.id.title_current)
     TextView mTextViewTitleCurrent;
@@ -51,52 +60,37 @@ public class MainActivity extends AppCompatActivity {
     ImageView mImageBtnOn;
     @BindView(R.id.switch_off)
     ImageView mImageBtnOff;
+    @Inject
+    PreferenceIsTracking mIsActive;
+    @BindView(R.id.status_tracking_off)
+    ImageView mLedTrackingOff;
+    @BindView(R.id.status_tracking_on)
+    ImageView mLedTrackingOn;
 
     private CompositeDisposable resumeDisposables;
     private GestureDetectorCompat mDetector;
     private Animation mSwitchAnimationFadeOut;
     private Animation mSwitchAnimationFadeIn;
     private boolean isSwitching;
+    @BindView(R.id.status_tracking)
+    ImageView mLedTracking;
+    private LocationsService mLocationsService;
+    @NonNull
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       @NonNull IBinder binder) {
+            mLocationsService = ((LocationsService.LocationsServiceBinder) binder).getService();
+            mLocationsService.removeFromForeground();
+            updateLedTrackingUpdating();
+        }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (BuildConfig.DEBUG) {
-            if (resumeDisposables != null) {
-                ErrorsObservable.notify(new Exception("resumeDisposables != null"));
-            }
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mLocationsService = null;
+            updateLedTrackingUpdating();
         }
-        resumeDisposables = new CompositeDisposable();
-        resumeDisposables.add(mUserStrategyFrequency
-                .subject
-                .subscribe(userStrategy -> updateUserStrategyChooser()));
-        updateUserStrategyChooser();
-    }
-
-    @Override
-    protected void onPause() {
-        if (resumeDisposables == null) {
-            ErrorsObservable.notify(new Exception("resumeDisposables == null"));
-        } else {
-            resumeDisposables.clear();
-            resumeDisposables = null;
-        }
-        super.onPause();
-    }
-
-    @OnTouch(R.id.gesture_controller)
-    public boolean detectGesture(MotionEvent event) {
-        if (mDetector.onTouchEvent(event)) {
-            if (BuildConfig.DEBUG) {
-                Log.d(LT, "R.id.gesture_controller OnTouch gesture detected");
-            }
-            return true;
-        }
-        if (BuildConfig.DEBUG) {
-            Log.d(LT, "R.id.gesture_controller OnTouch gesture not detected");
-        }
-        return super.onTouchEvent(event);
-    }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,9 +110,75 @@ public class MainActivity extends AppCompatActivity {
         mSwitchAnimationFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(this, LocationsService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        if (BuildConfig.DEBUG) {
+            if (resumeDisposables != null) {
+                ErrorsObservable.notify(new Exception("resumeDisposables != null"));
+            }
+        }
+        resumeDisposables = new CompositeDisposable();
+        resumeDisposables.add(mUserStrategyFrequency
+                .subject
+                .subscribe(userStrategy -> updateUserStrategyChooser()));
+        resumeDisposables.add(mIsActive
+                .subject
+                .subscribe(userStrategy -> updateSwitch()));
+        resumeDisposables.add(LocationsService
+                .subjectTracking
+                .subscribe(active -> updateLedTrackingUpdating()));
+        updateUserStrategyChooser();
+        updateSwitch();
+        updateLedTrackingUpdating();
+    }
+
+    @Override
+    protected void onPause() {
+        if (resumeDisposables == null) {
+            ErrorsObservable.notify(new Exception("resumeDisposables == null"));
+        } else {
+            resumeDisposables.clear();
+            resumeDisposables = null;
+        }
+        if (mIsActive.isOn()) {
+            LocationsService.startService(this, true);
+        }
+        if (mLocationsService != null) {
+            unbindService(mServiceConnection);
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus != mHasFocus) {
+            mHasFocus = hasFocus;
+        }
+    }
+
+    @OnTouch(R.id.gesture_controller)
+    public boolean detectGesture(MotionEvent event) {
+        if (mDetector.onTouchEvent(event)) {
+            if (BuildConfig.DEBUG) {
+                Log.d(LT, "R.id.gesture_controller OnTouch gesture detected");
+            }
+            return true;
+        }
+        if (BuildConfig.DEBUG) {
+            Log.d(LT, "R.id.gesture_controller OnTouch gesture not detected");
+        }
+        return super.onTouchEvent(event);
+    }
+
     @OnClick(R.id.switch_on)
     public void switchOn(View view) {
         if (isSwitching) return;
+        startService();
         MediaPlayerUtils.getInstance().playSwitchSound(this);
         isSwitching = true;
         mImageBtnOff.startAnimation(mSwitchAnimationFadeIn);
@@ -131,10 +191,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                mImageBtnOff.setVisibility(View.VISIBLE);
-                mImageBtnOn.setVisibility(View.GONE);
                 isSwitching = false;
                 mSwitchAnimationFadeOut.setAnimationListener(null);
+                mIsActive.set(true);
             }
 
             @Override
@@ -147,6 +206,7 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.switch_off)
     public void switchOff(View view) {
         if (isSwitching) return;
+        stopService();
         MediaPlayerUtils.getInstance().playSwitchSound(this);
         isSwitching = true;
         mImageBtnOff.startAnimation(mSwitchAnimationFadeOut);
@@ -159,10 +219,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                mImageBtnOff.setVisibility(View.GONE);
-                mImageBtnOn.setVisibility(View.VISIBLE);
                 isSwitching = false;
                 mSwitchAnimationFadeOut.setAnimationListener(null);
+                mIsActive.set(false);
             }
 
             @Override
@@ -183,5 +242,45 @@ public class MainActivity extends AppCompatActivity {
         mTextViewTitleNext1.setText(current.getTitleResID(1));
         mTextViewTitleNext2.setText(current.getTitleResID(2));
         mTextViewTitleNext3.setText(current.getTitleResID(3));
+    }
+
+    private void updateSwitch() {
+        if (mIsActive.isOn()) {
+            mImageBtnOff.setVisibility(View.VISIBLE);
+            mImageBtnOn.setVisibility(View.GONE);
+        } else {
+            mImageBtnOff.setVisibility(View.GONE);
+            mImageBtnOn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void updateLedTrackingUpdating() {
+        if (mLocationsService == null) {
+            mLedTracking.setVisibility(View.VISIBLE);
+            mLedTrackingOff.setVisibility(View.GONE);
+            mLedTrackingOn.setVisibility(View.GONE);
+        } else {
+            if (mLocationsService.isUpdatingLocation()) {
+                mLedTracking.setVisibility(View.GONE);
+                mLedTrackingOff.setVisibility(View.GONE);
+                mLedTrackingOn.setVisibility(View.VISIBLE);
+            } else {
+                mLedTracking.setVisibility(View.GONE);
+                mLedTrackingOff.setVisibility(View.VISIBLE);
+                mLedTrackingOn.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void startService() {
+        if (mLocationsService != null) {
+            mLocationsService.start(false);
+        }
+    }
+
+    private void stopService() {
+        if (mLocationsService != null) {
+            mLocationsService.stop();
+        }
     }
 }
