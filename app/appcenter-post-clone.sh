da086af1d7bf22ab4c9d7f167155229316606846
Sergey Dolin <sergey@s4y.solutions>
2019-02-07 04:23:23 +0500

    remove debug output

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -x
-pwd
+echo "GS"
 echo $GS
 echo $GS > "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
+echo "cat"
+cat "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
+echo $GS |sed 's/\\"/"/g' > "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
+echo "sed"
+cat "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
