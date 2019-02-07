f92dc5640a8c68409b4656da3fb1cd2538167967
Sergey Dolin <sergey@s4y.solutions>
2019-02-08 03:16:00 +0500

    Add secret java to appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-echo "GS"
-echo $GS
-echo $GS > "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
-echo "cat"
-cat "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
 echo $GS |sed 's/\\"/"/g' > "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
-echo "sed"
-cat "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
+
+cat > "$APPCENTER_SOURCE_DIRECTORY/app/src/main/java/solutions/s4y/waytoday/wsse/Secret.java" <<EOT
+package solutions.s4y.waytoday.wsse;
+
+import androidx.annotation.NonNull;
+
+public class Secret {
+    @NonNull
+    static String get(){
+        return "$SECRET";
+    };
+}
+EOT
